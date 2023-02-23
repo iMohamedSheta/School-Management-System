ff20c813512e93b1d842cd0b0a0a15ce11372a05
iMohamedSheta <mohamed15.sheta15@gmail.com>
2023-02-23 02:25:40 +0200

    Solvedgit st Navbar doesn't open in mobile screen in Arabic langauge. (Confilict And Solve, 2 css classes with the same name -> created prefix for dash-translate-x-full class in dashboard css. )

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,9 @@ import 'flowbite';
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 window.Alpine = Alpine;
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Alpine.plugin(focus);
 
 Alpine.start();
+import "flowbite/dist/flowbite";
--- resources/views/layouts/ar/main-sidebar.blade.php
+++ resources/views/layouts/ar/main-sidebar.blade.php
@@ -1,7 +1,7 @@
     <!-- sidenav  -->
-    {{-- <aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 mr-4 block w-full translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:right-0 xl:translate-x-0 xl:bg-transparent"> --}}
+    <aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 mr-4 block w-full dash-translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:right-0 xl:translate-x-0 xl:bg-transparent">
 
-    <aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 left-0 my-4 mr-4 block w-full  -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:right-0 xl:translate-x-0 xl:bg-transparent">
+    {{-- <aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 left-0 my-4 mr-4 block w-full  -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:right-0 xl:translate-x-0 xl:bg-transparent"> --}}
         <div class="h-19.5">
           <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close></i>
           <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="../pages/dashboard.html" target="_blank">
@@ -13,7 +13,7 @@
         <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
 
         <!-- change h-sidenav-no-pro to h-sidenav when pro is up -->
-        <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
+        <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav-no-pro grow basis-full">
           <ul class="flex flex-col pl-0 mb-0">
                 <li class="mt-0.5 w-full">
                     <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors {{ Route::currentRouteName("master") ? 'shadow-soft-xl rounded-lg bg-white font-semibold text-slate-700' : '' }}"
