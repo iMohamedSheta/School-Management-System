76b0079c96d00735a71090d7efe2d748d7c74fe6
iMohamedSheta <mohamed15.sheta15@gmail.com>
2023-02-26 00:42:16 +0200

    Added Lang/ar Arabic Values for laravel10.

--- lang/ar/passwords.php
+++ lang/ar/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'تمت إعادة تعيين كلمة المرور',
+    'sent' => 'تم إرسال تفاصيل استعادة كلمة المرور الخاصة بك إلى بريدك الإلكتروني',
+    'throttled' => 'الرجاء الانتظار قبل إعادة المحاولة',
+    'token' => 'رمز استعادة كلمة المرور هذا غير صالح',
+    'user' => "لم يتم العثور على أيّ حسابٍ بهذا العنوان الإلكتروني",
 
 ];
