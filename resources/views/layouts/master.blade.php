a06ca3625721210a67eb3346f7d06af3471f1fec
iMohamedSheta <mohamed15.sheta15@gmail.com>
2023-02-25 18:19:04 +0200

    Changed layouts SubFolder names from (ar,en) to (rtl,ltr) and Added comments about how to add more language as you need to you can set if it's Right to left language or left to Right Language.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{App::getlocale()}}" >
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png" />
     <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" >
     <title>{{ config('app.name') }} - Dashboard </title>
 
     {{-- ----------- Links ------------- --}}
@@ -25,17 +26,19 @@
                                 {{------------------------ Sidebar  ----------------------------------}}
 
     @switch(App::getlocale())
-
+                                    {{-- Right to left language put them with case ar --}}
+    {{-- @case('Right to left language') --}}
         @case('ar')
-            @include('layouts.ar.main-sidebar')
+            @include('layouts.rtl.main-sidebar')
         @break
-
+                                    {{-- Left to right language put them with case en --}}
+    {{-- @case('Left to right language') --}}
         @case('en')
-            @include('layouts.en.main-sidebar')
+            @include('layouts.ltr.main-sidebar')
         @break
-
+                                        {{-- Default Language (Only Choose One!) --}}
     @default
-        @include('layouts.en.main-sidebar')
+        @include('layouts.ltr.main-sidebar')
 
     @endswitch
 
@@ -45,15 +48,17 @@
             {{------------------------------------------------------ Main Tag  ------------------------------------------------------------}}
 
         @switch(App::getlocale())
-
+                                    {{-- Right to left language put them with case ar --}}
+    {{-- @case('Right to left language') --}}
             @case('ar')
                 <main id="mainRight" class="ease-soft-in-out xl:mr-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
             @break
-
+                                    {{-- Left to right language put them with case en --}}
+    {{-- @case('Left to right language') --}}
             @case('en')
                 <main id="mainLeft" class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
             @break
-
+                                        {{-- Default Language (Only Choose One!) --}}
         @default
             <main id="mainLeft" class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
 
@@ -64,17 +69,19 @@
                                 {{------------------------ Navbar  ----------------------------------}}
 
         @switch(App::getlocale())
-
+                                    {{-- Right to left language put them with case ar --}}
+    {{-- @case('Right to left language') --}}
             @case('ar')
-                @include('layouts.ar.main-header')
+                @include('layouts.rtl.main-header')
             @break
-
+                                    {{-- Left to right language put them with case en --}}
+    {{-- @case('Left to right language') --}}
             @case('en')
-                @include('layouts.en.main-header')
+                @include('layouts.ltr.main-header')
             @break
-
+                                        {{-- Default Language (Only Choose One!) --}}
         @default
-            @include('layouts.en.main-header')
+            @include('layouts.ltr.main-header')
 
         @endswitch
                             {{------------------------ END Navbar  ----------------------------------}}
@@ -101,17 +108,19 @@
 
                                 {{------------------------ Footer  ----------------------------------}}
         @switch(App::getlocale())
-
+                                    {{-- Right to left language put them with case en --}}
+    {{-- @case('Right to left language') --}}
             @case('ar')
-                @include('layouts.ar.footer')
+                @include('layouts.rtl.footer')
             @break
-
+                                    {{-- Left to right language put them with case en --}}
+    {{-- @case('Left to right language') --}}
             @case('en')
-                @include('layouts.en.footer')
+                @include('layouts.ltr.footer')
             @break
-
+                                        {{-- Default Language (Only Choose One!) --}}
         @default
-            @include('layouts.en.footer')
+            @include('layouts.ltr.footer')
 
         @endswitch
         {{------------------------ End Footer  --------------------------------}}
@@ -121,9 +130,11 @@
 
 
 
-                        {{----------------------------------------------- Footer scripts  ----------------------------------------------------}}
-                    @include('layouts.footer-scripts')
-                        {{-------------------------------------------  End Footer scripts  ----------------------------------------------------}}
+            {{----------------------------------------------- Footer scripts  ----------------------------------------------------}}
+        @include('layouts.footer-scripts')
+            {{-------------------------------------------  End Footer scripts  ----------------------------------------------------}}
+
+
 
 </body>
 
